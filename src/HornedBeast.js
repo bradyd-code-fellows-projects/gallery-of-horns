83735cbcbbbbd2b85b1e8c15cd6c0eca75deec6b
Brady <brady.davenport@pm.me>
2022-05-09 17:56:23 -0600

    constructing indiv HornedBeast objs

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -3,7 +3,11 @@ import React from 'react';
 class HornedBeast extends React.Component {
   render() {
     return (
-      <h2>Horned Beast</h2>
+      <article>
+        <h2>{this.props.name}</h2>
+        <img src={this.props.img} alt={this.props.alt} title={this.props.title}></img>
+        <p>{this.props.description}</p>
+      </article>
     )
   }
 }
