aa8941f3fc31feeb306cbb1a6e220f587065440b
Brady <brady.davenport@pm.me>
2022-05-10 17:08:06 -0600

    grid layout of imgs, center title

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import './Header.css'
 
 class Header extends React.Component {
 
   render() {
     return (
       <header>
-        Gallery of Horns
+        <h1>Gallery of Horns</h1>
       </header>
     );
   }
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './HornedBeasts.css'
 
 class HornedBeast extends React.Component {
   render() {
