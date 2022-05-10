fe2f1800a3787f1b894e61d9f5634ca7d57ca4df
Brady <brady.davenport@pm.me>
2022-05-10 16:13:27 -0600

    adds photo gallery to home page

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -4,11 +4,11 @@ class HornedBeast extends React.Component {
   render() {
     return (
       <article>
-        <h2>{this.props.name}</h2>
+        <h2>{this.props.title}</h2>
         <img
-        src={this.props.img}
+        src={this.props.image_url}
         alt={this.props.alt}
-        title={this.props.title} 
+        title={this.props.imgTitle} 
         />
         <p>{this.props.description}</p>
       </article>
